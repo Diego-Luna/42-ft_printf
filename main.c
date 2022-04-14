a35524832e05a398c7ef716ddbcefbb49c6022f9
Diego-Luna <diegofcolunalopez@gmail.com>
2022-04-13 20:24:01 -0500

    agregando pruevas

--- main.c
+++ main.c
@@ -6,39 +6,19 @@
 /*   By: diegofranciscolunalopez <diegofrancisco    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 13:06:55 by diegofranci       #+#    #+#             */
-/*   Updated: 2022/04/11 19:12:50 by diegofranci      ###   ########.fr       */
+/*   Updated: 2022/04/12 18:29:47 by diegofranci      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "src/ft_printf.h"
 #include <stdio.h>
 
-// Driver Code
-int main()
+int	main(void)
 {
-//    printf("\n\n Variadic functions: \n");
- 
-    // Variable number of arguments
-/*    printf("\n 1 + 2 = %d ",
-           AddNumbers(2, 1, 2));
- 
-    printf("\n 3 + 4 + 5 = %d ",
-           AddNumbers(3, 3, 4, 5));
- 
-    printf("\n 6 + 7 + 8 + 9 = %d ",
-           AddNumbers(4, 6, 7, 8, 9));
- 
-    printf("\n"); */
- 
 	printf("\n\n ft_printf functions: \n");
-
-	// Variable number of arguments
-    printf("\n hola + mundo = %d ",
-           ft_AddStringd("Hola", "Mundo"));
-
-    printf("\n");
-
-    return 0;
+	printf("\n hola = %d ", ft_printf("Hola mundo %s"));
+	printf("\n");
+	return (0);
 }
 
 /*
