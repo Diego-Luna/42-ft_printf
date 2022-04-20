2c4698eee629c7a09370af2c6490826b92c008e5
Diego-Luna <diegofcolunalopez@gmail.com>
2022-04-20 10:41:42 -0500

    Trabajando en el %p

--- src/ft_result/ft_result_x.c
+++ src/ft_result/ft_result_x.c
@@ -6,7 +6,7 @@
 /*   By: diegofranciscolunalopez <diegofrancisco    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/18 19:07:06 by diegofranci       #+#    #+#             */
-/*   Updated: 2022/04/19 08:33:37 by diegofranci      ###   ########.fr       */
+/*   Updated: 2022/04/20 10:32:14 by diegofranci      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,26 @@ void	ft_result_sign(t_printf *ob_print, int counter)
 	ft_update_result(ob_print, str, counter);
 }
 
+void	test(void *p)
+{
+	int	i;
+
+	i = 0;
+	while (p[i])
+	{
+		printf("--> v: %p", p[i]);
+		i++;
+	}
+}
+
 void	ft_result_p(void *p, t_printf *ob_print, int counter)
 {
-	printf("\n--2> 0> %p\n", p);
+	char	*p_char;
+
+	p_char = p;
+	test(&p);
+	printf("\n--2> 0> p: %p\n", p);
+	printf("\n--2> 0> d: %p\n", &p_char);
+	printf("\n--2> 0> convers: %s\n", ob_print->convers);
+	printf("\n--2> 0> counter: %d\n", counter);
 }
